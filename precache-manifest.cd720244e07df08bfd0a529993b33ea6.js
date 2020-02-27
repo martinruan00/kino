36f140588ac1e50c2820b075e4d08cd60b25efe4
The Quyet Nguyen <nguyenthequyet33@gmail.com>
2020-02-27 09:46:54 +0100

    Updates

---
+++ precache-manifest.cd720244e07df08bfd0a529993b33ea6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b3f148f345118cff55c81df6e862048",
+    "url": "/kino/index.html"
+  },
+  {
+    "revision": "173a5659b2883bba23f3",
+    "url": "/kino/static/css/main.808a91fc.chunk.css"
+  },
+  {
+    "revision": "f3d0e4a3b31e5c34105d",
+    "url": "/kino/static/js/2.c78b69fa.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/kino/static/js/2.c78b69fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "173a5659b2883bba23f3",
+    "url": "/kino/static/js/main.a9fc2664.chunk.js"
+  },
+  {
+    "revision": "3e8dff0b55306b67c153",
+    "url": "/kino/static/js/runtime-main.06c170c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kino/static/media/logo.5d5d9eef.svg"
+  }
+]);
